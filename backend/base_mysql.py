4a21c78ee57a4634d086b5cf6961ad18d8b9f03c
王书勤 <22373507@buaa.edu.cn>
2024-12-04 15:52:18 +0800

    refactor: vue3 to vue2

--- backend/base_mysql.py
+++ backend/base_mysql.py
@@ -1,5 +1,15 @@
 import pymysql
 
 class MYSQL:
-    def __init__(self):
-        return
+    def connectDatabase(self):
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='12345678',
+                                     db='spoc',
+                                     charset='utf8')
+        cursor = connection.cursor()
+        return connection, cursor
+
+    def closeDatabase(self, connection, cursor):
+        connection.close()
+        cursor.close()
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,40 +1,28 @@
 <template>
-  <NavBar />
-  <router-view />
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import NavBar from './components/NavBar.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar
+    HelloWorld
   }
 }
 </script>
 
 <style>
-/* #app {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-} */ 
 </style>
