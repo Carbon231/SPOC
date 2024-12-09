dae80262797036a7b891bdf99fe5a8856be2506f
sigridwicks <2724570552@qq.com>
2024-12-09 17:34:31 +0800

    feat(backend):add lottery and teacher post

--- backend/models.py
+++ backend/models.py
@@ -2,12 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=30, blank=True, null=True, unique=True, verbose_name='昵称')
-    avatar = models.CharField(max_length=30, default='', verbose_name='头像', blank=True, null=True)
-    birthday = models.CharField(max_length=30, default="2003-11-21", blank=True, null=True)
-    gender = models.CharField(max_length=30, default="女", blank=True, null=True)
-    email = models.CharField(max_length=30, blank=True, null=True)
-    hobbies = models.CharField(max_length=300, blank=True, null=True)
+    u_name = models.CharField(max_length=50)
 
 
 class Department(models.Model):
@@ -49,6 +44,8 @@ class Course(models.Model):
     c_name = models.CharField(max_length=30, blank=True, null=True, unique=True)
     teacher = models.ForeignKey(Teacher, on_delete=models.RESTRICT)
     intro = models.CharField(max_length=30, blank=True, null=True)
+    capacity = models.IntegerField(blank=True, null=True, default=30)
+    confirmed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.c_name
@@ -69,12 +66,13 @@ class SC(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     score = models.CharField(max_length=30, blank=True, null=True)
     hasScore = models.IntegerField(blank=True, null=True, default=0)
+    isSelect = models.IntegerField(blank=True, null=True, default=0)
 
     def __str__(self):
-        return self.score
+        return self.student.s_name
 
 class PostTheme(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.RESTRICT)
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
     title = models.CharField(max_length=30, blank=True, null=True)
     content = models.CharField(max_length=300, blank=True, null=True)
     time = models.CharField(max_length=30, blank=True, null=True)
@@ -85,7 +83,7 @@ class PostTheme(models.Model):
 
 
 class Post(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.RESTRICT)
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
     content = models.CharField(max_length=300, blank=True, null=True)
     time = models.CharField(max_length=30, blank=True, null=True)
     post_theme = models.ForeignKey(PostTheme, on_delete=models.CASCADE)
