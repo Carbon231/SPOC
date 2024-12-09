282a5f814f77b6c353d797e5af03bb20d76b2b2b
Lord-Admiral-JO <1729968989@qq.com>
2024-12-09 22:31:06 +0800

    feat: save Teacher-Comment(not done)

--- frontend/src/components/Teacher/TeacherCommentAndDiscuss/TeacherAllDiscuss.vue
+++ frontend/src/components/Teacher/TeacherCommentAndDiscuss/TeacherAllDiscuss.vue
@@ -30,9 +30,9 @@
                 </div>
                 <div class="textitem" style="font-size: 10px; margin-top: 2%; margin-bottom: 2%">
                   <el-tag size="mini">
-                    <span v-if="postTheme.isTeacher === 0">学生</span>
-                    <span v-else-if="postTheme.isTeacher === 1">教师</span>
-                    <span v-else-if="postTheme.isTeacher === 2">管理员</span>
+                    <span v-if="postTheme.isExcellent === 0">学生</span>
+                    <span v-else-if="postTheme.isExcellent === 1">教师</span>
+                    <span v-else-if="postTheme.isExcellent === 2">管理员</span>
                   </el-tag>
                 </div>
                 <div>
@@ -41,6 +41,41 @@
               </el-card>
             </el-col>
             <el-col :span="8" :offset="2" class="right-information">
+              <el-card shadow="hover" style="width: 100%">
+                <el-row>
+                  <el-col :span="11">
+                    <el-image :src="teacherImg" lazy></el-image>
+                  </el-col>
+                  <el-col :span="12" :offset="1">
+                    <el-descriptions :column="1">
+                      <el-descriptions-item label="用户名">{{ t_name }}</el-descriptions-item>
+                      <el-descriptions-item label="学号">{{ t_id }}</el-descriptions-item>
+                    </el-descriptions>
+                  </el-col>
+                </el-row>
+                <el-row>
+                  <el-divider></el-divider>
+                </el-row>
+                <el-row>
+                  <el-button @click="buildThemeVisible = true" style="width: 100%" type="primary">新建主题贴</el-button>
+                  <el-dialog title="新建主题帖" :visible.sync="buildThemeVisible" width="70%">
+                    <el-row style="margin-bottom: 10px">
+                      <el-col>
+                        <el-input v-model="input.title" placeholder="请输入标题"></el-input>
+                      </el-col>
+                    </el-row>
+                    <el-row style="margin-bottom: 10px">
+                      <el-col>
+                        <quill-editor ref="text" v-model="input.content" style="height: 300px"></quill-editor>
+                      </el-col>
+                    </el-row>
+                    <div slot="footer" class="dialog-footer" style="margin-top: 10%">
+                      <el-button @click="buildThemeVisible = false">取消</el-button>
+                      <el-button type="primary" @click="buildPostTheme">确定</el-button>
+                    </div>
+                  </el-dialog>
+                </el-row>
+              </el-card>
             </el-col>
           </el-row>
         </el-main>
@@ -52,7 +87,7 @@
 <script>
 import TeacherNav from '../TeacherNav'
 import TeacherHeading from '../TeacherHeading'
-// import { quillEditor } from 'vue-quill-editor'
+import { quillEditor } from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
@@ -60,20 +95,38 @@ import TeacherImg from '../../../assets/img/teacher.png'
 
 export default {
   name: 'TeacherAllDiscuss',
-  components: { TeacherNav, TeacherHeading },
+  components: { TeacherNav, TeacherHeading, quillEditor },
   data: function () {
     return {
       loading: true,
       t_id: '',
       t_name: '',
-      inputSearch: '',
+      teacherImg: TeacherImg,
       input: {
         title: '',
         content: ''
       },
-      teacherImg: TeacherImg,
-      postThemeList: [],
-      showPostThemeList: [],
+      inputSearch: '',
+      postThemeList: [{
+        pt_id: '',
+        s_id: '',
+        s_name: '',
+        title: '',
+        content: '',
+        time: '',
+        isExcellent: 0
+      }],
+      showPostThemeList: [
+        {
+          pt_id: '',
+          s_id: '',
+          s_name: '',
+          title: '',
+          content: '',
+          time: '',
+          isExcellent: 0
+        }
+      ],
       buildThemeVisible: false,
       time: ''
     }
@@ -82,7 +135,6 @@ export default {
     this.t_id = this.cookie.getCookie('t_id')
     this.t_name = this.cookie.getCookie('t_name')
     this.getPostThemeList()
-    this.getTeacherDiscussNum()
   },
   methods: {
     getPostThemeList: function () {
@@ -90,26 +142,19 @@ export default {
       that.loading = true
       this.$http.request({
         url: that.$url + 'GetPostThemeList/',
-        method: 'post'
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json'
+        },
       }).then(function (response) {
         console.log(response.data)
         that.loading = false
-        if (response.data.code === 200) {
-          that.postThemeList = response.data.data
-          that.showPostThemeList = response.data.data
-        } else {
-          that.$message.error(response.data.message)
-        }
+        that.postThemeList = response.data.data
+        that.showPostThemeList = response.data.data
       }).catch(function (error) {
         console.log(error)
         that.loading = false
       })
-    },
-    searchDiscuss: function (inputSearch) {
-      this.showPostThemeList = this.postThemeList.filter(postTheme => postTheme.title.includes(inputSearch))
-    },
-    enterPostTheme: function (index) {
-      this.$router.push({ name: 'TeacherDiscuss', params: { id: this.showPostThemeList[index].id } })
     },
     getTime: function () {
       let dt = new Date()
@@ -120,6 +165,70 @@ export default {
       let m = dt.getMinutes().toString().padStart(2, '0')
       let s = dt.getSeconds().toString().padStart(2, '0')
       this.time = yyyy + '-' + MM + '-' + dd + ' ' + h + ':' + m + ':' + s
+    },
+    buildPostTheme: function () {
+      let that = this
+      that.getTime()
+      this.$http.request({
+        url: that.$url + 'BuildPostTheme/',
+        method: 'post',
+        data: {
+          t_id: that.t_id,
+          title: that.input.title,
+          content: that.input.content,
+          time: that.time
+        },
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then(function (response) {
+        console.log(response.data)
+        if (response.data === 0) {
+          that.$message.success('创建成功')
+          that.buildThemeVisible = false
+          that.getPostThemeList()
+          that.input = {
+            title: '',
+            content: ''
+          }
+        } else {
+          that.$message.error('未知错误')
+        }
+      }).catch(function (error) {
+        console.log(error)
+      })
+    },
+    enterPostTheme: function (index) {
+      let that = this
+      this.$router.push({
+        name: 'TeacherDiscuss',
+        query: {
+          pt_id: that.showPostThemeList[index].pt_id,
+        }
+      })
+    },
+    goToHelloWorld: function () {
+      this.cookie.clearCookie('t_id')
+      this.cookie.clearCookie('t_name')
+      this.$router.replace('/')
+    },
+    searchDiscuss: function (inputSearch) {
+      this.showPostThemeList = this.searchByIndexOf(inputSearch, this.postThemeList)
+    },
+    searchByIndexOf: function (keyWord, list) {
+      if (!(list instanceof Array)) {
+        return
+      } else if (keyWord === '') {
+        return list
+      }
+      const len = list.length
+      const arr = []
+      for (let i = 0; i < len; i++) {
+        if (list[i].title.toString().includes(keyWord)) {
+          arr.push(list[i])
+        }
+      }
+      return arr
     }
   }
 }
